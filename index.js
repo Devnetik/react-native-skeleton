612efd533d4c2638244a35ed3920bda815fd3e58
Michael Malura <maluramichael@googlemail.com>
2016-08-16 17:20:00 +0200

    Remove initial boilerplate code

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by maluramichael on 16/08/16.
+ */
+
+import React, {Component} from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<View>
+				<Text>React Native Skeleton</Text>
+			</View>
+		);
+	}
+}
